0b574d4b06ddb1d3ee5e275a6f568754a8bc1495
Drew Williams <WilliamsAg2@hendrix.edu>
2026-04-29 13:23:19 -0500

    Pulling and testing it

---
+++ HendrixAdvancement/Pages/Proposal.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HendrixAdvancement.Pages;
+
+public class ProposalModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
