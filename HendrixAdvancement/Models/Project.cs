526b02752498826cf239f0823911f8fdd6f07605
Chris Bell <bellcn@hendrix.edu>
2026-04-29 18:38:22 -0500

    Removed Approval (didn't scaffold I think),  Database up on home page

--- HendrixAdvancement/Models/Project.cs
+++ HendrixAdvancement/Models/Project.cs
@@ -31,5 +31,4 @@ public class Project
     public Status Status { get; set; }
     public decimal Cost { get; set; }
     public YesOrNo Nameable { get; set; }
-    public YesOrNo Approval { get; set; }
 }
--- HendrixAdvancement/Pages/Index.cshtml.cs
+++ HendrixAdvancement/Pages/Index.cshtml.cs
@@ -1,12 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Microsoft.EntityFrameworkCore;
+using HendrixAdvancement.Data;
+using HendrixAdvancement.Models;
 namespace HendrixAdvancement.Pages;
 
 public class IndexModel : PageModel
 {
-    public void OnGet()
+
+    private readonly HendrixAdvancement.Data.HendrixAdvancementContext _context;
+
+    public IndexModel(HendrixAdvancement.Data.HendrixAdvancementContext context)
     {
+        _context = context;
+    }
+
+    public IList<Project> Project { get; set; } = default!;
+
+    public async Task OnGetAsync()
+    {
+        Project = await _context.Project.ToListAsync();
     }
 }
 
