48f6a3e9789034a47064a1df4ec89902374aa099
Chris Bell <bellcn@hendrix.edu>
2026-04-29 19:51:40 -0500

    Changes to Database Style
    Style changed on the homepage.

--- HendrixAdvancement/Models/Project.cs
+++ HendrixAdvancement/Models/Project.cs
@@ -21,7 +21,6 @@ public class Project
 {
     public int Id { get; set; }
     public string? Title { get; set; }
-    [DataType(DataType.Date)]
     public string? Description { get; set; }
     public string? Location { get; set; }
     public string? Department { get; set; }
--- HendrixAdvancement/Pages/Admin.cshtml.cs
+++ HendrixAdvancement/Pages/Admin.cshtml.cs
@@ -1,11 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using HendrixAdvancement.Data;
+using HendrixAdvancement.Models;
 
 namespace HendrixAdvancement.Pages;
 
 public class AdminModel : PageModel
 {
-    public void OnGet()
+    private readonly HendrixAdvancement.Data.HendrixAdvancementContext _context;
+
+    public AdminModel(HendrixAdvancement.Data.HendrixAdvancementContext context)
     {
+        _context = context;
+    }
+
+    public IList<Project> Project { get; set; } = default!;
+
+    public async Task OnGetAsync()
+    {
+        Project = await _context.Project.ToListAsync();
     }
 }
