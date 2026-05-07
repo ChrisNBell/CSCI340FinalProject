f751163d68c12e9605b3b46c881a3e7e611de523
Chris Bell <bellcn@hendrix.edu>
2026-05-07 14:46:53 -0500

    Fixed Details, Database Issues

---
+++ HendrixAdvancement/Pages/Projects/PublicDetails.cshtml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using HendrixAdvancement.Data;
+using HendrixAdvancement.Models;
+
+namespace HendrixAdvancement.Pages_Projects
+{
+    public class PublicDetailsModel : PageModel
+    {
+        private readonly HendrixAdvancement.Data.HendrixAdvancementContext _context;
+
+        public PublicDetailsModel(HendrixAdvancement.Data.HendrixAdvancementContext context)
+        {
+            _context = context;
+        }
+
+        public Project Project { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var project = await _context.Projects
+                .Include(s => s.Opportunities)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ProjectId == id);
+
+            if (project is not null)
+            {
+                Project = project;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+    }
+}
