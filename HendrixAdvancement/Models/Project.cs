c4265d2a205dd31f015e43759e300a2bfe0be2d0
Chris Bell <bellcn@hendrix.edu>
2026-05-08 14:05:12 -0500

    Changes to Admin Link

--- HendrixAdvancement/Models/Project.cs
+++ HendrixAdvancement/Models/Project.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace HendrixAdvancement.Models{
+namespace HendrixAdvancement.Models
+{
 
     public enum YesOrNo
     {
